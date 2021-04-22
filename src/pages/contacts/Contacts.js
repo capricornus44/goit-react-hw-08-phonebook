ac7eea6f9d69d73efb87ffecab90b2715e9624e7
capricornus44 <antonzavalniuk@gmail.com>
2021-04-22 23:07:54 +0200

    create basic application structure and code refactoring

---
+++ src/pages/contacts/Contacts.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { CSSTransition } from 'react-transition-group';
+import { ToastContainer } from 'react-toastify';
+import { getContacts } from '../../redux/phonebook/contact-operations';
+import { itemsSelector } from '../../redux/phonebook/contact-selectors';
+import Form from '../../components/form/Form';
+import Filter from '../../components/filter/Filter';
+import ContactList from '../../components/contactList/ContactList';
+import './Contacts.scss';
+
+class Contacts extends Component {
+  state = {
+    animation: false,
+  };
+
+  componentDidMount() {
+    this.props.onGetContacts();
+
+    this.setState(state => ({
+      animation: !state.animation,
+    }));
+  }
+
+  render() {
+    return (
+      <div className="app_container">
+        {/* APPLICATION TITLE */}
+        <CSSTransition
+          in={true}
+          appear={true}
+          classNames="title_slider"
+          timeout={500}
+          unmountOnExit
+        >
+          <h1 className="app_title">Phonebook</h1>
+        </CSSTransition>
+
+        {/* APPLICATION FORM */}
+        <section className="section">
+          <Form />
+        </section>
+
+        {/* CONTACTS FILTER */}
+        <CSSTransition
+          in={this.props.contacts.length > 1}
+          classNames="filter_animation"
+          timeout={250}
+          unmountOnExit
+        >
+          <section className="section">
+            <Filter />
+          </section>
+        </CSSTransition>
+
+        {/* CONTACTS LIST */}
+        <ContactList />
+
+        {/* ALERT NOTIFICATION */}
+        <ToastContainer autoClose={2500} position="top-right" type="error" />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  contacts: itemsSelector(state),
+});
+
+const mapDispatchToProps = {
+  onGetContacts: getContacts,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Contacts);
