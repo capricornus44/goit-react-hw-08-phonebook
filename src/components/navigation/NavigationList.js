0994f03814acfc6d5edfe243be6785c635d5e8d0
capricornus44 <antonzavalniuk@gmail.com>
2021-04-25 16:25:03 +0200

    Added condition for rendering of navigation links

--- src/components/navigation/NavigationList.js
+++ src/components/navigation/NavigationList.js
@@ -1,24 +1,56 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import { getIsAuthenticated } from '../../redux/auth/auth-selectors';
 import './NavigationList.scss';
 
 const NavigationList = ({ routes }) => {
+  const isAuthenticated = useSelector(getIsAuthenticated);
+
   return (
     <ul className="navigation_list">
-      {routes.map(({ name, path, exact }) => (
-        <li key={path} className="navigation_item">
-          <NavLink
-            exact={exact}
-            to={path}
-            className="navigation_link"
-            activeClassName="navigation_link--active"
-          >
-            {name}
-          </NavLink>
-        </li>
-      ))}
+      {isAuthenticated
+        ? routes.map(({ name, path, exact }) => (
+            <li key={path} className="navigation_item">
+              <NavLink
+                exact={exact}
+                to={path}
+                className="navigation_link"
+                activeClassName="navigation_link--active"
+              >
+                {name}
+              </NavLink>
+            </li>
+          ))
+        : routes
+            .filter(route => !route.private)
+            .map(({ name, path, exact }) => (
+              <li key={path} className="navigation_item">
+                <NavLink
+                  exact={exact}
+                  to={path}
+                  className="navigation_link"
+                  activeClassName="navigation_link--active"
+                >
+                  {name}
+                </NavLink>
+              </li>
+            ))}
     </ul>
   );
 };
 
 export default NavigationList;
+
+//map(({ name, path, exact }) => (
+//   <li key={path} className="navigation_item">
+//     <NavLink
+//       exact={exact}
+//       to={path}
+//       className="navigation_link"
+//       activeClassName="navigation_link--active"
+//     >
+//       {name}
+//     </NavLink>
+//   </li>
+// ))
