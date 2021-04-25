7f46db541b632decc51cea651bef4b4025839ba2
capricornus44 <antonzavalniuk@gmail.com>
2021-04-25 21:34:49 +0200

    Added condition for components route

--- src/components/navigation/NavigationRoutes.js
+++ src/components/navigation/NavigationRoutes.js
@@ -1,16 +1,43 @@
 import React, { Suspense } from 'react';
 import { Switch, Route } from 'react-router-dom';
 
+import { getIsAuthenticated } from '../../redux/auth/auth-selectors';
+import { useSelector } from 'react-redux';
+
 const NavigationRoutes = ({ routes }) => {
+  const isAuthenticated = useSelector(getIsAuthenticated); //
+
   return (
     <Suspense fallback={<h2 className="loader">...loading</h2>}>
       <Switch>
-        {routes.map(({ exact, path, component }) => (
-          <Route exact={exact} path={path} component={component} key={path} />
-        ))}
+        {isAuthenticated
+          ? routes.map(({ component, path, exact }) => (
+              <Route
+                exact={exact}
+                path={path}
+                component={component}
+                key={path}
+              />
+            ))
+          : routes
+              .filter(route => !route.private)
+              .map(({ component, path, exact }) => (
+                <Route
+                  exact={exact}
+                  path={path}
+                  component={component}
+                  key={path}
+                />
+              ))}
       </Switch>
     </Suspense>
   );
 };
 
 export default NavigationRoutes;
+
+// {
+//   routes.map(({ exact, path, component }) => (
+//     <Route exact={exact} path={path} component={component} key={path} />
+//   ));
+// }
